0ed8816edf987c419a3fc7dc29583b849faab10a
Paul Vorbach <paul@vorba.ch>
2014-04-14 11:21:20 +0200

    Pix and PixColormap

--- src/main/java/de/vorb/tesseract/bridj/Tesseract.java
+++ src/main/java/de/vorb/tesseract/bridj/Tesseract.java
@@ -8,6 +8,8 @@ import org.bridj.CRuntime;
 import org.bridj.FlagSet;
 import org.bridj.IntValuedEnum;
 import org.bridj.Pointer;
+import org.bridj.StructObject;
+import org.bridj.ann.Field;
 import org.bridj.ann.Library;
 import org.bridj.ann.Name;
 import org.bridj.ann.Ptr;
@@ -1342,10 +1344,283 @@ public class Tesseract {
 
   };
 
-  /** Undefined type */
-  public static interface Pix {
+  /** Leptonica Pix */
+  public static class Pix extends StructObject {
+    /** width in pixels */
+    @Field(0)
+    public int w() {
+      return this.io.getIntField(this, 0);
+    }
 
-  };
+    /** width in pixels */
+    @Field(0)
+    public Pix w(int w) {
+      this.io.setIntField(this, 0, w);
+      return this;
+    }
+
+    /** height in pixels */
+    @Field(1)
+    public int h() {
+      return this.io.getIntField(this, 1);
+    }
+
+    /** height in pixels */
+    @Field(1)
+    public Pix h(int h) {
+      this.io.setIntField(this, 1, h);
+      return this;
+    }
+
+    /** depth in bits (bpp) */
+    @Field(2)
+    public int d() {
+      return this.io.getIntField(this, 2);
+    }
+
+    /** depth in bits (bpp) */
+    @Field(2)
+    public Pix d(int d) {
+      this.io.setIntField(this, 2, d);
+      return this;
+    }
+
+    /** number of samples per pixel */
+    @Field(3)
+    public int spp() {
+      return this.io.getIntField(this, 3);
+    }
+
+    /** number of samples per pixel */
+    @Field(3)
+    public Pix spp(int spp) {
+      this.io.setIntField(this, 3, spp);
+      return this;
+    }
+
+    /** 32-bit words/line */
+    @Field(4)
+    public int wpl() {
+      return this.io.getIntField(this, 4);
+    }
+
+    /** 32-bit words/line */
+    @Field(4)
+    public Pix wpl(int wpl) {
+      this.io.setIntField(this, 4, wpl);
+      return this;
+    }
+
+    /** reference count (1 if no clones) */
+    @Field(5)
+    public int refcount() {
+      return this.io.getIntField(this, 5);
+    }
+
+    /** reference count (1 if no clones) */
+    @Field(5)
+    public Pix refcount(int refcount) {
+      this.io.setIntField(this, 5, refcount);
+      return this;
+    }
+
+    /** image res (ppi) in x direction */
+    @Field(6)
+    public int xres() {
+      return this.io.getIntField(this, 6);
+    }
+
+    /** image res (ppi) in x direction */
+    @Field(6)
+    public Pix xres(int xres) {
+      this.io.setIntField(this, 6, xres);
+      return this;
+    }
+
+    /**
+     * (use 0 if unknown)<br>
+     * image res (ppi) in y direction
+     */
+    @Field(7)
+    public int yres() {
+      return this.io.getIntField(this, 7);
+    }
+
+    /**
+     * (use 0 if unknown)<br>
+     * image res (ppi) in y direction
+     */
+    @Field(7)
+    public Pix yres(int yres) {
+      this.io.setIntField(this, 7, yres);
+      return this;
+    }
+
+    /**
+     * (use 0 if unknown)<br>
+     * input file format, IFF_*
+     */
+    @Field(8)
+    public int informat() {
+      return this.io.getIntField(this, 8);
+    }
+
+    /**
+     * (use 0 if unknown)<br>
+     * input file format, IFF_*
+     */
+    @Field(8)
+    public Pix informat(int informat) {
+      this.io.setIntField(this, 8, informat);
+      return this;
+    }
+
+    /** special instructions for I/O, etc */
+    @Field(9)
+    public int special() {
+      return this.io.getIntField(this, 9);
+    }
+
+    /** special instructions for I/O, etc */
+    @Field(9)
+    public Pix special(int special) {
+      this.io.setIntField(this, 9, special);
+      return this;
+    }
+
+    /**
+     * text string associated with pix<br>
+     * C type : char*
+     */
+    @Field(10)
+    public Pointer<Byte> text() {
+      return this.io.getPointerField(this, 10);
+    }
+
+    /**
+     * text string associated with pix<br>
+     * C type : char*
+     */
+    @Field(10)
+    public Pix text(Pointer<Byte> text) {
+      this.io.setPointerField(this, 10, text);
+      return this;
+    }
+
+    /**
+     * colormap (may be null)<br>
+     * C type : PixColormap*
+     */
+    @Field(11)
+    public Pointer<PixColormap> colormap() {
+      return this.io.getPointerField(this, 11);
+    }
+
+    /**
+     * colormap (may be null)<br>
+     * C type : PixColormap*
+     */
+    @Field(11)
+    public Pix colormap(Pointer<PixColormap> colormap) {
+      this.io.setPointerField(this, 11, colormap);
+      return this;
+    }
+
+    /**
+     * the image data<br>
+     * C type : unsigned int*
+     */
+    @Field(12)
+    public Pointer<Integer> data() {
+      return this.io.getPointerField(this, 12);
+    }
+
+    /**
+     * the image data<br>
+     * C type : unsigned int*
+     */
+    @Field(12)
+    public Pix data(Pointer<Integer> data) {
+      this.io.setPointerField(this, 12, data);
+      return this;
+    }
+
+    public Pix() {
+      super();
+    }
+
+    public Pix(Pointer<Pix> pointer) {
+      super(pointer);
+    }
+  }
+
+  public static class PixColormap extends StructObject {
+    /**
+     * colormap table (array of RGBA_QUAD)<br>
+     * C type : void*
+     */
+    @Field(0)
+    public Pointer<?> array() {
+      return this.io.getPointerField(this, 0);
+    }
+
+    /**
+     * colormap table (array of RGBA_QUAD)<br>
+     * C type : void*
+     */
+    @Field(0)
+    public PixColormap array(Pointer<?> array) {
+      this.io.setPointerField(this, 0, array);
+      return this;
+    }
+
+    /** of pix (1, 2, 4 or 8 bpp) */
+    @Field(1)
+    public int depth() {
+      return this.io.getIntField(this, 1);
+    }
+
+    /** of pix (1, 2, 4 or 8 bpp) */
+    @Field(1)
+    public PixColormap depth(int depth) {
+      this.io.setIntField(this, 1, depth);
+      return this;
+    }
+
+    /** number of color entries allocated */
+    @Field(2)
+    public int nalloc() {
+      return this.io.getIntField(this, 2);
+    }
+
+    /** number of color entries allocated */
+    @Field(2)
+    public PixColormap nalloc(int nalloc) {
+      this.io.setIntField(this, 2, nalloc);
+      return this;
+    }
+
+    /** number of color entries used */
+    @Field(3)
+    public int n() {
+      return this.io.getIntField(this, 3);
+    }
+
+    /** number of color entries used */
+    @Field(3)
+    public PixColormap n(int n) {
+      this.io.setIntField(this, 3, n);
+      return this;
+    }
+
+    public PixColormap() {
+      super();
+    }
+
+    public PixColormap(Pointer<PixColormap> pointer) {
+      super(pointer);
+    }
+  }
 
   /** Undefined type */
   public static interface FILE {
