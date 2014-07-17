4ae2dd116193fe4b7185319caecb34ae419e6040
Paul Vorbach <paul@vorba.ch>
2014-07-17 21:06:42 +0200

    Prefer int over signed bytes
    The actual values should be unsigned

--- src/main/java/de/vorb/tesseract/INT_FEATURE_STRUCT.java
+++ src/main/java/de/vorb/tesseract/INT_FEATURE_STRUCT.java
@@ -12,66 +12,63 @@ public class INT_FEATURE_STRUCT extends CPPObject {
     }
 
     @Constructor(0)
+    public INT_FEATURE_STRUCT() {
+        super();
+    }
+
+    @Constructor(1)
     public INT_FEATURE_STRUCT(Pointer<FCOORD> pos, byte theta) {
         super((Void) null, 0, pos, theta);
     }
 
-    @Constructor(1)
+    @Constructor(2)
     public INT_FEATURE_STRUCT(int x, int y, int theta) {
         super((Void) null, 1, x, y, theta);
     }
 
     @Field(0)
-    public byte X() {
-        return this.io.getByteField(this, 0);
+    public int X() {
+        return this.io.getByteField(this, 0) & 0xFF;
     }
 
     @Field(0)
-    public INT_FEATURE_STRUCT X(byte X) {
-        this.io.setByteField(this, 0, X);
+    public INT_FEATURE_STRUCT X(int X) {
+        this.io.setByteField(this, 0, (byte) X);
         return this;
     }
 
     @Field(1)
-    public byte Y() {
-        return this.io.getByteField(this, 1);
+    public int Y() {
+        return this.io.getByteField(this, 1) & 0xFF;
     }
 
     @Field(1)
-    public INT_FEATURE_STRUCT Y(byte Y) {
-        this.io.setByteField(this, 1, Y);
+    public INT_FEATURE_STRUCT Y(int Y) {
+        this.io.setByteField(this, 1, (byte) Y);
         return this;
     }
 
     @Field(2)
-    public byte Theta() {
-        return this.io.getByteField(this, 2);
+    public int Theta() {
+        return this.io.getByteField(this, 2) & 0xFF;
     }
 
     @Field(2)
-    public INT_FEATURE_STRUCT Theta(byte Theta) {
-        this.io.setByteField(this, 2, Theta);
+    public INT_FEATURE_STRUCT Theta(int Theta) {
+        this.io.setByteField(this, 2, (byte) Theta);
         return this;
     }
 
     @Field(3)
-    public byte CP_misses() {
-        return this.io.getByteField(this, 3);
+    public int CP_misses() {
+        return this.io.getByteField(this, 3) & 0xFF;
     }
 
     @Field(3)
-    public INT_FEATURE_STRUCT CP_misses(byte CP_misses) {
-        this.io.setByteField(this, 3, CP_misses);
+    public INT_FEATURE_STRUCT CP_misses(int CP_misses) {
+        this.io.setByteField(this, 3, (byte) CP_misses);
         return this;
     }
 
     public native void print();
-
-    public INT_FEATURE_STRUCT() {
-        super();
-    }
-
-    public INT_FEATURE_STRUCT(Pointer<INT_FEATURE_STRUCT> pointer) {
-        super(pointer);
-    }
 }
